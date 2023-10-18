6f368316be12d62314269db7aca5afaf651abe09
Milian Solberg <milianhs@outlook.com>
2023-10-18 09:32:59 +0200

    sm chng

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+};
